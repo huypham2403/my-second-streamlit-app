e772c81707552808555c44c72489dbbefa1c30ef
Phạm Lê Tường Huy <124581563+huypham2403@users.noreply.github.com>
2023-03-17 21:48:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,22 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import pickle
+df = pd.read_csv('https://raw.githubusercontent.com/huypham2403/my-second-streamlit-app/main/IceCreamData.csv')
+df.head()
+x = df['Temperature'].values
+y = df['Revenue'].values
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=50)
+model = LinearRegression()
+x = x.reshape(-1,1)
+model.fit(x, y)
 st.title('Revenue Prediction')
 a = st.number_input('Input Temperature')
 if st.button('Predict'):
     a = np.array(a).reshape(-1,1)
-    df = pd.read_csv('https://raw.githubusercontent.com/huypham2403/my-second-streamlit-app/main/IceCreamData.csv')
-    df.head()
-    x = df['Temperature'].values
-    y = df['Revenue'].values
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=50)
-    model = LinearRegression()
-    x = x.reshape(-1,1)
-    model.fit(x, y)
     y_pred = model.predict(a)
     st.caption('Revenue Prediction')
     st.success(y_pred)
+filename = 'my-second-streamlit-app-main'
+pickle.dump(model, open(filename, "wb"))
+model = pickle.load(open(filename, "rb"))
